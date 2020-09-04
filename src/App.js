83feba2229e6c0829cb7aea1b316892aba989577
Andrey Simonov <sasmeo@gmail.com>
2020-09-05 05:02:33 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {createGlobalStyle} from 'styled-components';
+import styled from 'styled-components';
+import { NavBar } from './Components/NavBar';
+
+const GlobalStyle = createGlobalStyle`
+  html{
+    box-sizing: border-box;
+  }
+  *,
+  *::before,
+  *::after{
+    box-sizing: inherit;
+  }
+  body{
+    margin: 0;
+    background-color: black;
+    color: white;
+    font-family: Roboto, sens-serif;
+    font-size: 20px;
+  }
+  img{
+    max-width: 100%;
+    heigth: auto;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+  ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  h1,h2,h3,h4{
+    font-family: Pacifico, sens-serif;
+    padding: 0;
+    margin: 0;
+  }
+  p{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+      <NavBar/>
+      
+    </>
+    
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../image/logo.svg'
+
+const NavBarStyled = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    height: 80px;
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    background-color: #299B01;
+    color: white;
+`;
+
+const Logo = styled.div`
+    display: flex;
+`;
+
+const H1 = styled.h1`
+    font-size: 24px;
+    margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+    width: 50px;
+`;
+
+const ButtonLogin = styled.button`
+    box-shadow:inset 0px 1px 0px 0px #fff6af;
+	background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+	background-color:#ffec64;
+	border-radius:6px;
+	border:1px solid #ffaa22;
+	display:inline-block;
+	cursor:pointer;
+	color:#333333;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffee66;
+    outline: 0;
+    :hover{
+        background:linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+	    background-color:#ffab23;
+    }
+    :active{
+        position:relative;
+	    top:1px;
+        outline: 0;
+    }
+`;
+
+export const NavBar = () => (
+    <NavBarStyled>
+        <ImgLogo src={logoImg} alt="" />
+        <H1>MrQuad's</H1>
+        <ButtonLogin>войти</ButtonLogin>
+    </NavBarStyled>
+)
